b4b0ec6fdcfef6081c2d8926170eeb73bf97d0eb
king <aclengen@gmail.com>
2024-12-10 15:33:01 -0500

    updated for 4.4

--- addons/gdscript_formatter/gdscript_formatter.gd
+++ addons/gdscript_formatter/gdscript_formatter.gd
@@ -1,41 +1,20 @@
-'''
-	MIT License
-
-	Copyright (c) 2024-present 忘忧の (Daylily Zeleen) - <daylily-zeleen@foxmail.com>
-
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
-
-	The above copyright notice and this permission notice shall be included in all
-	copies or substantial portions of the Software.
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-	SOFTWARE.
-'''
-
 @tool
 extends EditorPlugin
 
 const _GDScriptFormatterPreference = preload("scripts/preference.gd")
 
-var _preference
+var _preference: Resource
 var _has_format_tool_item: bool = false
 var _has_install_update_tool_item: bool = false
 var _install_task_id: int = -1
 var _connection_list: Array[Resource] = []
+var _script_editor: ScriptEditor = EditorInterface.get_script_editor()
 
 
 func _init() -> void:
-	var preference_res_file = (get_script() as Resource).resource_path.get_base_dir().path_join("format_preference.tres")
+	var preference_res_file = (get_script() as Resource).resource_path.get_base_dir().path_join(
+		"format_preference.tres"
+	)
 	if not FileAccess.file_exists(preference_res_file):
 		_preference = _GDScriptFormatterPreference.new()
 		ResourceSaver.save(_preference, preference_res_file)
@@ -45,7 +24,9 @@ func _init() -> void:
 	# Update script for plugin updating, then reload it.
 	if _preference.script.resource_path != (_GDScriptFormatterPreference as Script).resource_path:
 		# Get old properies
-		var props = (_preference.get_script() as Script).get_script_property_list().filter(func(p): return p["usage"] & PROPERTY_USAGE_STORAGE != 0)
+		var props = (_preference.get_script() as Script).get_script_property_list().filter(
+			func(p): return p["usage"] & PROPERTY_USAGE_STORAGE != 0
+		)
 		props = props.map(func(p): return {"name": p["name"], "value": _preference.get(p["name"])})
 		# Apply old properties
 		_preference = _GDScriptFormatterPreference.new()
@@ -53,7 +34,9 @@ func _init() -> void:
 			if prop["name"] in _preference:
 				_preference.set(prop["name"], prop["value"])
 		# Apply old shortcut if valid
-		var old_shortcut_file := (get_script() as Resource).resource_path.get_base_dir().path_join("format_shortcut.tres")
+		var old_shortcut_file := (get_script() as Resource).resource_path.get_base_dir().path_join(
+			"format_shortcut.tres"
+		)
 		if FileAccess.file_exists(old_shortcut_file):
 			_preference.shortcut = load(old_shortcut_file).duplicate(true)
 			DirAccess.remove_absolute(old_shortcut_file)
@@ -71,7 +54,9 @@ func _enter_tree() -> void:
 		_print_warning('"%s" is required for installing "gdtoolkit".' % _get_pip_command())
 		_print_warning("\tPlease install it and ensure it can be found in your environment.")
 	else:
-		add_tool_menu_item("GDScriptFormatter: Install/Update gdtoolkit", install_or_update_gdtoolkit)
+		add_tool_menu_item(
+			"GDScriptFormatter: Install/Update gdtoolkit", install_or_update_gdtoolkit
+		)
 		_has_install_update_tool_item = true
 
 	update_shortcut()
@@ -86,7 +71,7 @@ func _exit_tree() -> void:
 func _shortcut_input(event: InputEvent) -> void:
 	if not _has_format_tool_item:
 		return
-	var shortcut = _get_shortcut()
+	var shortcut: Shortcut = _get_shortcut()
 	if not is_instance_valid(shortcut):
 		return
 	if shortcut.matches_event(event) and event.is_pressed() and not event.is_echo():
@@ -95,14 +80,14 @@ func _shortcut_input(event: InputEvent) -> void:
 
 
 func format_script() -> bool:
-	if not EditorInterface.get_script_editor().is_visible_in_tree():
+	if not _script_editor.is_visible_in_tree():
 		return false
-	var current_script = EditorInterface.get_script_editor().get_current_script()
+	var current_script: Script = _script_editor.get_current_script()
 	if not is_instance_valid(current_script) or not current_script is GDScript:
 		return false
-	var code_edit: CodeEdit = EditorInterface.get_script_editor().get_current_editor().get_base_editor()
+	var code_edit: CodeEdit = _script_editor.get_current_editor().get_base_editor()
 
-	var formatted := []
+	var formatted: Array[String] = []
 	if not _format_code(current_script.resource_path, code_edit.text, formatted):
 		return false
 
@@ -115,9 +100,16 @@ func install_or_update_gdtoolkit() -> void:
 		_print_warning("Installing or updating gdformat, please be patient.")
 		return
 	if not _has_command(_get_pip_command()):
-		printerr('Installation of GDScript Formatter failed: Command "%s" is required, please ensure it can be found in your environment.' % _get_pip_command())
+		printerr(
+			(
+				'Installation of GDScript Formatter failed: Command "%s" is required, please ensure it can be found in your environment.'
+				% _get_pip_command()
+			)
+		)
 		return
-	_install_task_id = WorkerThreadPool.add_task(_install_or_update_gdtoolkit, true, "Install or update gdtoolkit.")
+	_install_task_id = WorkerThreadPool.add_task(
+		_install_or_update_gdtoolkit, true, "Install or update gdtoolkit."
+	)
 	while _install_task_id >= 0:
 		if not WorkerThreadPool.is_task_completed(_install_task_id):
 			await get_tree().process_frame
@@ -131,7 +123,7 @@ func update_shortcut() -> void:
 
 	_connection_list.clear()
 
-	var shortcut = _get_shortcut()
+	var shortcut: Shortcut = _get_shortcut()
 	if is_instance_valid(shortcut):
 		for event in shortcut.events:
 			event = event as InputEvent
@@ -145,7 +137,9 @@ func update_shortcut() -> void:
 
 func _on_resource_saved(resource: Resource) -> void:
 	# Preference
-	var preference_path = (get_script() as Resource).resource_path.get_base_dir().path_join("format_preference.tres")
+	var preference_path: String = (get_script() as Resource).resource_path.get_base_dir().path_join(
+		"format_preference.tres"
+	)
 	if resource.resource_path == preference_path:
 		_preference = load(preference_path)
 		return
@@ -154,14 +148,14 @@ func _on_resource_saved(resource: Resource) -> void:
 	if not _preference.format_on_save:
 		return
 
-	var gds := resource as GDScript
+	var gds: GDScript = resource as GDScript
 	if resource == get_script():
 		return
 
 	if not _has_format_tool_item or not is_instance_valid(gds):
 		return
 
-	var formatted := []
+	var formatted: Array[String] = []
 	if not _format_code(gds.resource_path, gds.source_code, formatted):
 		return
 
@@ -169,33 +163,40 @@ func _on_resource_saved(resource: Resource) -> void:
 	ResourceSaver.save(gds)
 	gds.reload()
 
-	var script_editor := get_editor_interface().get_script_editor()
-	var open_script_editors := script_editor.get_open_script_editors()
-	var open_scripts := script_editor.get_open_scripts()
+	var open_script_editors: Array[ScriptEditorBase] = _script_editor.get_open_script_editors()
+	var open_scripts: Array[Script] = _script_editor.get_open_scripts()
 
 	if not open_scripts.has(gds):
 		return
 
-	if script_editor.get_current_script() == gds:
-		_reload_code_edit(script_editor.get_current_editor().get_base_editor(), formatted.back(), true)
+	if _script_editor.get_current_script() == gds:
+		_reload_code_edit(
+			_script_editor.get_current_editor().get_base_editor(), formatted.back(), true
+		)
 	elif open_scripts.size() == open_script_editors.size():
 		for i in range(open_scripts.size()):
 			if open_scripts[i] == gds:
 				_reload_code_edit(open_script_editors[i].get_base_editor(), formatted.back(), true)
 				return
 	else:
-		printerr("GDScript Formatter error: Unknown situation, can't reload code editor in Editor. Please report this issue.")
+		printerr(
+			"GDScript Formatter error: Unknown situation, can't reload code editor in Editor. Please report this issue."
+		)
 
 
 func _install_or_update_gdtoolkit():
-	var has_gdformat = _has_command(_get_gdformat_command())
+	var has_gdformat: bool = _has_command(_get_gdformat_command())
 	if has_gdformat:
 		print("-- Beginning gdtoolkit update.")
 	else:
 		print("-- Beginning gdtoolkit installation.")
 
-	var output := []
-	var err := OS.execute(_get_pip_command(), ["install", "gdtoolkit"], output)
+	var output: Array[String] = []
+	var err: int = OS.execute(
+		_get_pip_command(),
+		["install", "git+https://github.com/Scony/godot-gdscript-toolkit.git"],
+		output
+	)
 	if err == OK:
 		if has_gdformat:
 			print("-- Update of gdtoolkit successful.")
@@ -215,14 +216,27 @@ func _add_format_tool_item_and_command() -> void:
 	if _has_format_tool_item:
 		return
 	if not _has_command(_get_gdformat_command()):
-		_print_warning('GDScript Formatter: The command "%s" can\'t be found in your environment.' % _get_gdformat_command())
+		_print_warning(
+			(
+				'GDScript Formatter: The command "%s" can\'t be found in your environment.'
+				% _get_gdformat_command()
+			)
+		)
 		_print_warning('\tIf you have not installed "gdtoolkit", install it first.')
-		_print_warning('\tIf you have installed "gdtoolkit", change "gdformat_command" to a valid command in "%s", and save this resource.' % _preference.resource_path)
+		_print_warning(
+			(
+				'\tIf you have installed "gdtoolkit", change "gdformat_command" to a valid command in "%s", and save this resource.'
+				% _preference.resource_path
+			)
+		)
 		return
 	add_tool_menu_item("GDScriptFormatter: Format script", format_script)
 	var shortcut = _get_shortcut()
 	EditorInterface.get_command_palette().add_command(
-		"Format GDScript", "GDScript Formatter/Format GDScript", format_script, shortcut.get_as_text() if is_instance_valid(shortcut) else "None"
+		"Format GDScript",
+		"GDScript Formatter/Format GDScript",
+		format_script,
+		shortcut.get_as_text() if is_instance_valid(shortcut) else "None"
 	)
 	_has_format_tool_item = true
 
@@ -236,87 +250,111 @@ func _remove_format_tool_item_and_command() -> void:
 
 
 func _has_command(command: String) -> bool:
-	var output := []
-	var err := OS.execute(command, ["--version"], output)
+	var output: Array[String] = []
+	var err: int = OS.execute(command, ["--version"], output)
 
 	return err == OK
 
 
 func _reload_code_edit(code_edit: CodeEdit, new_text: String, tag_saved: bool = false) -> void:
-	var caret_column := code_edit.get_caret_column()
-	var caret_line := code_edit.get_caret_line()
-	var scroll_hor := code_edit.scroll_horizontal
-	var scroll_ver := code_edit.scroll_vertical
+	var carent_column: int = code_edit.get_caret_column()
+	var carent_line: int = code_edit.get_caret_line()
+	var scroll_hor: int = code_edit.scroll_horizontal
+	var scroll_ver: float = code_edit.scroll_vertical
 
-	# Breakpoints
-	var breakpoints := _store_code_edit_info(code_edit.get_breakpointed_lines, code_edit.get_line)
+	var breakpoints: Dictionary[int, int] = _store_code_edit_info(
+		code_edit.get_breakpointed_lines, code_edit.get_line
+	)
 	# Bookmarks
-	var bookmarks := _store_code_edit_info(code_edit.get_bookmarked_lines, code_edit.get_line)
+	var bookmarks: Dictionary[int, int] = _store_code_edit_info(
+		code_edit.get_bookmarked_lines, code_edit.get_line
+	)
 	# Folds
-	var folds := _store_code_edit_info(code_edit.get_folded_lines, code_edit.get_line)
+	var folds: Dictionary[int, int] = _store_code_edit_info(
+		code_edit.get_folded_lines, code_edit.get_line
+	)
 
-	# New text
 	code_edit.text = new_text
 	if tag_saved:
 		code_edit.tag_saved_version()
 
-	var new_text_line_count := code_edit.get_line_count()
+	var new_text_line_count: int = code_edit.get_line_count()
 	# Breakpoints
-	_restore_code_edit_info(breakpoints, code_edit.get_line, code_edit.set_line_as_breakpoint, new_text_line_count)
+	_restore_code_edit_info(
+		breakpoints, code_edit.get_line, code_edit.set_line_as_breakpoint, new_text_line_count
+	)
 	# Bookmarks
-	_restore_code_edit_info(bookmarks, code_edit.get_line, code_edit.set_line_as_bookmarked, new_text_line_count)
+	_restore_code_edit_info(
+		bookmarks, code_edit.get_line, code_edit.set_line_as_bookmarked, new_text_line_count
+	)
 	# Folds
-	_restore_code_edit_info(folds, code_edit.get_line, func(line: int, _1: bool) -> void: code_edit.fold_line(line), new_text_line_count)
+	_restore_code_edit_info(
+		folds,
+		code_edit.get_line,
+		func(line: int, _1: bool) -> void: code_edit.fold_line(line),
+		new_text_line_count
+	)
 
-	code_edit.set_caret_column(caret_column)
-	code_edit.set_caret_line(caret_line)
+	code_edit.set_caret_column(carent_column)
+	code_edit.set_caret_line(carent_line)
 	code_edit.scroll_horizontal = scroll_hor
 	code_edit.scroll_vertical = scroll_ver
-
 	code_edit.update_minimum_size()
 
 
-func _store_code_edit_info(func_get_lines: Callable, func_get_line: Callable) -> Dictionary:
-	var ret := {}
+func _store_code_edit_info(
+	func_get_lines: Callable, func_get_line: Callable
+) -> Dictionary[int, int]:
+	var ret: Dictionary[int, int] = {}
 	for line in func_get_lines.call():
 		ret[line] = func_get_line.call(line)
 	return ret
 
 
-func _restore_code_edit_info(prev_data: Dictionary, func_get_line: Callable, func_set_line: Callable, new_text_line_count: int) -> void:
-	var prev_lines := PackedInt64Array(prev_data.keys())
+func _restore_code_edit_info(
+	prev_data: Dictionary,
+	func_get_line: Callable,
+	func_set_line: Callable,
+	new_text_line_count: int
+) -> void:
+	var prev_lines: PackedInt64Array = PackedInt64Array(prev_data.keys())
 	for idx in range(prev_lines.size()):
-		var prev_line := prev_lines[idx] as int
-		var prev_text := prev_data[prev_line] as String
-
+		var prev_line: int = prev_lines[idx]
+		var prev_text: String = prev_data[prev_line]
 		if func_get_line.call(prev_line).similarity(prev_text) > 0.9:
 			func_set_line.call(prev_line, true)
-
-		var up_line := prev_line - 1
-		var down_line := prev_line + 1
+		var up_line: int = prev_line - 1
+		var down_line: int = prev_line + 1
 		while up_line >= 0 or down_line < new_text_line_count:
-			if down_line < new_text_line_count and func_get_line.call(down_line).similarity(prev_text) > 0.9:
+			if (
+				down_line < new_text_line_count
+				and func_get_line.call(down_line).similarity(prev_text) > 0.9
+			):
 				func_set_line.call(down_line, true)
 				break
 			if up_line >= 0 and func_get_line.call(up_line).similarity(prev_text) > 0.9:
 				func_set_line.call(up_line, true)
 				break
-
 			up_line -= 1
 			down_line += 1
 
 
-func _format_code(script_path: String, code: String, formated: Array) -> bool:
-	const tmp_file = "res://addons/gdscript_formatter/.tmp.gd"
-	var f = FileAccess.open(tmp_file, FileAccess.WRITE)
+func _format_code(script_path: String, code: String, formated: Array[String]) -> bool:
+	const tmp_file: String = "res://addons/gdscript_formatter/.tmp.gd"
+	var line_length: int = EditorInterface.get_editor_settings().get(
+		"text_editor/appearance/guidelines/line_length_guideline_hard_column"
+	)
+	var f: FileAccess = FileAccess.open(tmp_file, FileAccess.WRITE)
 	if not is_instance_valid(f):
 		printerr("GDScript Formatter Error: Can't create tmp file.")
 		return false
 	f.store_string(code)
 	f.close()
 
-	var output := []
-	var args := [ProjectSettings.globalize_path(tmp_file), "--line-length=%d" % _preference.line_length]
+	var output: Array[String] = []
+	var args: Array[String] = [
+		ProjectSettings.globalize_path(tmp_file), "--line-length=%d" % line_length
+	]
 	if _preference.fast_but_unsafe:
 		args.push_back("--fast")
 	var err = OS.execute(_get_gdformat_command(), args, output)
@@ -327,7 +365,9 @@ func _format_code(script_path: String, code: String, formated: Array) -> bool:
 	else:
 		printerr("Format GDScript failed: ", script_path)
 		printerr("\tExit code: ", err, " Output: ", output.front().strip_edges())
-		printerr('\tIf your script does not have any syntax errors, this error is led by limitations of "gdtoolkit", e.g. multiline lambda.')
+		printerr(
+			'\tIf your script does not have any syntax errors, this error is led by limitations of "gdtoolkit", e.g. multiline lambda.'
+		)
 
 	DirAccess.remove_absolute(tmp_file)
 	return err == OK
@@ -345,5 +385,5 @@ func _get_shortcut() -> Shortcut:
 	return _preference.shortcut
 
 
-func _print_warning(str: String) -> void:
-	print_rich("[color=orange]%s[/color]" % str)
+func _print_warning(warning: String) -> void:
+	print_rich("[color=orange]%s[/color]" % warning)
--- addons/gdscript_formatter/scripts/preference.gd
+++ addons/gdscript_formatter/scripts/preference.gd
@@ -1,42 +1,18 @@
-'''
-	MIT License
-
-	Copyright (c) 2024-present 忘忧の (Daylily Zeleen) - <daylily-zeleen@foxmail.com>
-
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
-
-	The above copyright notice and this permission notice shall be included in all
-	copies or substantial portions of the Software.
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-	SOFTWARE.
-'''
-
 @tool
 extends Resource
 
-var _version := "v0.2.0"
+var _version: String = "v0.2.0"
 
-## How many characters per line to allow.
-## 每行允许的最大字符数量。
-@export var line_length := 175:
-	set(v):
-		line_length = v
-		emit_changed()
+### How many characters per line to allow.
+### 每行允许的最大字符数量。
+#@export var line_length: int = 175:
+#set(v):
+#line_length = v
+#emit_changed()
 
 ## If true, will format on save.
 ## 如果开启，将在脚本保存时进行格式化。
-@export var format_on_save := false:
+@export var format_on_save: bool = false:
 	set(v):
 		format_on_save = v
 		emit_changed()
@@ -45,7 +21,7 @@ var _version := "v0.2.0"
 ## Default is "Shift+Alt+F"。
 ## 格式化脚本所使用的快捷键
 ## 默认为"Shift+Alt+F"。
-@export var shortcut := _create_default_shortcut():
+@export var shortcut: Shortcut = _create_default_shortcut():
 	set(v):
 		if is_instance_valid(shortcut) and shortcut.changed.is_connected(emit_changed):
 			shortcut.changed.disconnect(emit_changed)
@@ -56,21 +32,21 @@ var _version := "v0.2.0"
 
 ## If true, will skip safety checks.
 ## 如果开启，则跳过安全检查。
-@export var fast_but_unsafe := false:
+@export var fast_but_unsafe: bool = false:
 	set(v):
 		fast_but_unsafe = v
 		emit_changed()
 
 ## The gdformat command to use on the command line, you might need to modify this option if the "gdformat" is not installed for all users.
 ## 用于格式化的gdformat命令，如果你的gdformat不是为所有用户安装时可能需要修改该选项。
-@export var gdformat_command := "gdformat":
+@export var gdformat_command: String = "gdformat":
 	set(v):
 		gdformat_command = v
 		emit_changed()
 
 ## The pip command to use on the command line, you might need to modify this option if the "python/pip" is not installed for all users.
 ## 用于安装/更新gdformat而使用的pip命令，如果你的python/pip不是为所有用户安装时可能需要修改该选项。
-@export var pip_command := "pip":
+@export var pip_command: String = "pip":
 	set(v):
 		pip_command = v
 		emit_changed()
@@ -81,14 +57,14 @@ func _init() -> void:
 
 
 func _create_default_shortcut() -> Shortcut:
-	var default_shortcut := InputEventKey.new()
+	var default_shortcut: InputEventKey = InputEventKey.new()
 	default_shortcut.echo = false
 	default_shortcut.pressed = true
 	default_shortcut.keycode = KEY_F
 	default_shortcut.shift_pressed = true
 	default_shortcut.alt_pressed = true
 
-	var shortcut = Shortcut.new()
-	shortcut.events.push_back(default_shortcut)
+	var _shortcut: Shortcut = Shortcut.new()
+	_shortcut.events.push_back(default_shortcut)
 
-	return shortcut
+	return _shortcut
